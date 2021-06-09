807f92609f29535072ff039983f267deb58623c3
Adolfo <adolfoyanes@gmail.com>
2021-06-09 17:05:18 -0500

    fix_url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,9 @@
 # $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDIS_URL"])
 REDIS = Redis.new(:url => uri)
 
 if Rails.env == 'production'
-  uri = URI.parse(ENV["REDISTOGO_URL"])
+  uri = URI.parse(ENV["REDIS_URL"])
   REDIS = Redis.new(:url => uri)
 end
