d015e3120f06538748651e604500e77b56f4c75d
Gaspar Chuc <gaschuc@gmail.com>
2021-06-09 22:36:29 -0500

    Redis prueba 5

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -12,4 +12,4 @@ end
 
 #url = "redis://:p7094fc34870884a8d8cf8495f2f28d5206cadad3c62f873caba8d00d809bb54e@ec2-3-210-158-128.compute-1.amazonaws.com:30000"
 #url.scheme = "rediss"
-$redis = Redis.new(url: "#{ENV["REDIS_URL")
+$redis = Redis.new(url: ENV["REDIS_URL"])
