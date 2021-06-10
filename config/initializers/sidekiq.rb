ff72a75657bbfb09680b53ae6786c980b83798cd
Gaspar Chuc <gaschuc@gmail.com>
2021-06-09 20:37:04 -0500

    Prueba redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
+url = URI.parse("redis://:p7094fc34870884a8d8cf8495f2f28d5206cadad3c62f873caba8d00d809bb54e@ec2-3-210-158-128.compute-1.amazonaws.com:30000")
+url.scheme = "rediss"
+url.port = Integer(url.port) + 1
+
 Sidekiq.configure_server do |config|
-    config.redis = { url: "#{ENV["REDIS_URL"]}/1" }
+    config.redis = { url: url.to_s }
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { url: "#{ENV["REDIS_URL"]}/1" }
+    config.redis = { url: url.to_s }
 end
