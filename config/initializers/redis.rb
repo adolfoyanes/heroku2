d7e042b6d769eaca02139a23ab44e91d36743af4
Gaspar Chuc <gaschuc@gmail.com>
2021-06-09 19:52:59 -0500

    Edición del archivo redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
 # $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 
+=begin
 uri = URI.parse("#{ENV["REDIS_URL"]}/1")
 REDIS = Redis.new(:url => uri)
 
@@ -7,3 +8,9 @@ if Rails.env == 'production'
   uri = URI.parse("#{ENV["REDIS_URL"]}/1")
   REDIS = Redis.new(:url => uri)
 end
+=end
+
+url = URI.parse(ENV["REDIS_URL"])
+url.scheme = "rediss"
+url.port = Integer(url.port) + 1
+$redis = Redis.new(url: url, driver: :ruby, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
