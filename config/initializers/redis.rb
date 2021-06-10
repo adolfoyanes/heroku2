c0474b48b3ed152c461d2ee6ac57593943cd92d4
Gaspar Chuc <gaschuc@gmail.com>
2021-06-09 22:30:40 -0500

    Redis prueba 4

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -10,6 +10,6 @@ if Rails.env == 'production'
 end
 =end
 
-url = URI.parse("redis://:p7094fc34870884a8d8cf8495f2f28d5206cadad3c62f873caba8d00d809bb54e@ec2-3-210-158-128.compute-1.amazonaws.com:30000")
-url.scheme = "rediss"
-$redis = Redis.new(url: url)
+#url = "redis://:p7094fc34870884a8d8cf8495f2f28d5206cadad3c62f873caba8d00d809bb54e@ec2-3-210-158-128.compute-1.amazonaws.com:30000"
+#url.scheme = "rediss"
+$redis = Redis.new(url: "#{ENV["REDIS_URL")
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,7 @@
-url = URI.parse("redis://:p7094fc34870884a8d8cf8495f2f28d5206cadad3c62f873caba8d00d809bb54e@ec2-3-210-158-128.compute-1.amazonaws.com:30000")
-url.scheme = "rediss"
-url.port = Integer(url.port)
-
 Sidekiq.configure_server do |config|
-    config.redis = { url: url.to_s }
+    config.redis = { url: "#{ENV["REDIS_URL"}
 end
 
 Sidekiq.configure_client do |config|
-    config.redis = { url: url.to_s }
+    config.redis = { url: "#{ENV["REDIS_URL" }
 end
