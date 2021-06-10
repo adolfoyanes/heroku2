1bf50791c20ab73c23ff5896bc4820f7628a57bd
Gaspar Chuc <gaschuc@gmail.com>
2021-06-09 20:41:43 -0500

    Prueba redis 2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -12,5 +12,5 @@ end
 
 url = URI.parse("redis://:p7094fc34870884a8d8cf8495f2f28d5206cadad3c62f873caba8d00d809bb54e@ec2-3-210-158-128.compute-1.amazonaws.com:30000")
 url.scheme = "rediss"
-url.port = Integer(url.port) + 1
+url.port = Integer(url.port)
 $redis = Redis.new(url: url, driver: :ruby, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
