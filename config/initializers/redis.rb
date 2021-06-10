ba8eedc21935aff6a19844a48fd93e34095da000
Gaspar Chuc <gaschuc@gmail.com>
2021-06-09 23:47:35 -0500

    Prueba redis 10

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,12 +2,12 @@
 
 
 uri = URI.parse(ENV["REDIS_URL"])
-REDIS = Redis.new(:url => uri)
+REDIS = Redis.new(:url => uri,ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 
-if Rails.env == 'production'
-  uri = URI.parse(ENV["REDIS_URL"])
-  REDIS = Redis.new(:url => uri)
-end
+#if Rails.env == 'production'
+#  uri = URI.parse(ENV["REDIS_URL"])
+#  REDIS = Redis.new(:url => uri)
+#end
 
 
 #url = "redis://:p7094fc34870884a8d8cf8495f2f28d5206cadad3c62f873caba8d00d809bb54e@ec2-3-210-158-128.compute-1.amazonaws.com:30000"
