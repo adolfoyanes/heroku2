cab8e3c271b87728e6ffe1a499a5cdf76b942ee4
Gaspar Chuc <gaschuc@gmail.com>
2021-06-09 19:38:35 -0500

    Cambio en uri

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,9 @@
 # $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 
-uri = URI.parse("#{ENV["REDIS_URL"]}/15")
+uri = URI.parse("#{ENV["REDIS_URL"]}/1")
 REDIS = Redis.new(:url => uri)
 
 if Rails.env == 'production'
-  uri = URI.parse("#{ENV["REDIS_URL"]}/15")
+  uri = URI.parse("#{ENV["REDIS_URL"]}/1")
   REDIS = Redis.new(:url => uri)
 end
